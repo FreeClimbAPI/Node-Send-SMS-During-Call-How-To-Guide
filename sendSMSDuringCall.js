627a681ec378e6bfcd19fc77f9de228becf5bc36
Francis San Filippo <fsanfilipp@vailsys.com>
2019-09-16 14:42:15 -0500

    initial commit

---
+++ sendSMSDuringCall.js
@@ -0,0 +1,36 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const app = express()
+app.use(bodyParser.json())
+const persephonySDK = require('@persephony/sdk')
+
+const port = process.env.PORT || 3000
+const host = process.env.HOST
+const accountId = process.env.accountId
+const authToken = process.env.authToken
+const persephony = persephonySDK(accountId, authToken)
+
+app.post('/incomingCall', (req, res) => {
+  const options = {
+    notificationUrl: `${host}/notificationUrl`
+  }
+  // Create sms PerCL that sends sms to current caller using the number handling the request
+  const smsCommand = persephony.percl.sms(req.body.to, req.body.from, 'Incoming Phone Call', options)
+  const percl = persephony.percl.build(smsCommand)
+  res.status(200).json(percl)
+})
+
+// Receive status updates of the sms
+app.post('/notificationUrl', (req, res) => {
+  console.log('Outbound Message Status Change: ', req.body)
+})
+
+// Specify this route with 'Status Callback URL' in App Config
+app.post('/status', (req, res) => {
+  // handle status changes
+  res.status(200)
+})
+
+app.listen(port, () => {
+  console.log(`Starting server on ${port}`)
+})
